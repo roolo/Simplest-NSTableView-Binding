0ecd3b627fa55335822b0ea5179c616994883c36
Mailo Svetel <development@rooland.cz>
2012-03-06 19:12:14 +0100

    Initial Commit

---
+++ Simplest NSTableView Binding/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Simplest NSTableView Binding
+//
+//  Created by Milan Svetel on 3/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Simplest NSTableView Binding/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  Simplest NSTableView Binding
+//
+//  Created by Milan Svetel on 3/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
